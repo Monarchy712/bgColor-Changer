282aa200bdb0852c1897168d5acd7bac6357a727
Samyaak <samyaakjain712@gmail.com>
2026-03-26 15:10:57 +0530

    push project

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('olive')
+
+  function changeColor(color){
+    setColor(color)
+  }
+
+  return (
+    <div className='w-full h-screen duration-200' style={{backgroundColor:color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button
+            onClick={() =>  changeColor("red")}
+            className='outline-none px-4 py-1 bg-red-500 rounded-full shadow-lg text-black'
+          >Red</button>
+          <button
+            onClick={() =>  changeColor("blue")}
+            className='outline-none px-4 py-1 bg-blue-500 rounded-full shadow-lg text-black'
+          >Blue</button>
+          <button
+            onClick={() =>  changeColor("green")}
+            className='outline-none px-4 py-1 bg-green-500 rounded-full shadow-lg text-black'
+          >Green</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
